b45350dabf3dba69d7d6de81ae14666f50cc24bb
Leon van der Noll <leonvdnoll@gmail.com>
2022-03-10 16:14:07 +0100

    codes can now be dupes

--- card_tracker_app/models/set.py
+++ card_tracker_app/models/set.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Set(models.Model):
-    code = models.CharField(max_length=255, unique=True, null=True)
+    code = models.CharField(max_length=255, unique=False, null=True)
     name = models.CharField(max_length=255, unique=True)
     cards_per_row = models.IntegerField(default=3)
 
